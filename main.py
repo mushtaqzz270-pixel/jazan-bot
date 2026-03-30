554bc1f6f7ac0605b1a1cb985654fdd16341c478
mushtaqzz270-pixel <mushtaqzz270@gmail.com>
2026-03-30 05:26:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Bot is starting...")
+import asyncio
+from telethon import TelegramClient, events
+
+api_id = 34136368
+api_hash = "a53fbec24918979bf1af188f47d850ae"
+
+client = TelegramClient("session", api_id, api_hash)
+
+@client.on(events.NewMessage)
+async def handler(event):
+    print("New message:", event.text)
+
+async def main():
+    await client.start()
+    print("Bot is running...")
+
+    await client.run_until_disconnected()
+
+asyncio.run(main())
